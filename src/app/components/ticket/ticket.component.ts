c100bc96b87676817ddd89922d35552c77a884b7
JuanAlejandroCR <_juan_alejandro@outlook.com>
2023-03-27 14:17:50 -0600

    Vali

--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -39,8 +39,32 @@ export class TicketComponent {
       alert("SUCCESS")
     }
   }
-  validar_aspirante(){
-    
+  validar_aspirante() {
+    const telefono = this.registerForm.controls.telefono.value;
+    const celular = this.registerForm.controls.celular.value;
+    const correo = this.registerForm.controls.correo.value;
+    const curp = this.registerForm.controls.curp.value;
+  
+    const telefonoRegex = /^[0-9]{10}$/;
+    const correoRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const curpRegex = /^[A-Z]{4}[0-9]{6}[HM][A-Z]{5}[0-9]{2}$/;
+  
+    if (!telefonoRegex.test(telefono)) {
+      alert('El campo de teléfono debe tener 10 dígitos numéricos');
+    }
+    else if (!telefonoRegex.test(celular)) {
+      alert('El campo de celular debe tener 10 dígitos numéricos');
+    }
+    else if (!correoRegex.test(correo)) {
+      alert('El campo de correo electrónico debe tener un formato válido');
+    }
+    else if (!curpRegex.test(curp)) {
+      alert('El campo de CURP debe tener un formato válido');
+    }
+    else {
+      alert('Todos los campos son válidos');
+    }
   }
+  
 
 }
