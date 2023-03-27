7fc5c48db02b7bb615447b4b9858883105573df0
AlfonsoLoopez <loopezalfonso@gmail.com>
2023-03-27 10:14:46 -0600

    Añadiendo componente de interfaz de admin

---
+++ src/app/components/inter-admin/inter-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inter-admin',
+  templateUrl: './inter-admin.component.html',
+  styleUrls: ['./inter-admin.component.scss']
+})
+export class InterAdminComponent {
+
+}
