b67379f140f5936ac80ca71ee83136fe326a4106
FerrerFerrer <antonioferrer@uadec.edu.mx>
2023-03-28 13:11:12 -0600

    todo asalvo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { TicketComponent } from './components/ticket/ticket.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { MenuTopbarComponent } from './components/menu-topbar/menu-topbar.component';
+import { InterAdminComponent } from './components/inter-admin/inter-admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RecaptchaModule, RECAPTCHA_SETTINGS } from 'ng-recaptcha';
 
 @NgModule({
@@ -14,24 +17,22 @@ import { RecaptchaModule, RECAPTCHA_SETTINGS } from 'ng-recaptcha';
     AppComponent,
     TicketComponent,
     LoginComponent,
-    MenuTopbarComponent
+    MenuTopbarComponent,
+    InterAdminComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    RecaptchaModule,
+    RecaptchaModule
   ],
-  providers: [
-    { 
-      provide: RECAPTCHA_SETTINGS,
-      useValue: '6LcWByglAAAAAOWXTSiXQo7ve_uipxdKe5j1J8wm'
-
-
-    }
-
-  ],
+  providers: [{ 
+    provide: RECAPTCHA_SETTINGS,
+    useValue: '6LcWByglAAAAAOWXTSiXQo7ve_uipxdKe5j1J8wm'
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
