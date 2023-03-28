144703cd4988d211ae1ff806eed920b1ef482d7c
FerrerFerrer <antonioferrer@uadec.edu.mx>
2023-03-28 13:02:46 -0600

    primer error

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,14 @@ export class LoginComponent {
   captcha: string;
   email: string;
 
-  constructor(){
+ res: any;
+  admin = {
+    email: "",
+    password: ""
+  }
+ datosIncorrectos = false;
+
+  constructor( private router: Router){
     this.captcha = '';
     this.email = 'Secret@email';
   }
@@ -22,4 +30,36 @@ export class LoginComponent {
 
     }
 
+async validarAdmin() {
+    let url = "http://localhost:8080/api/admin/";
+    let data = {
+      method: "GET",
+      headers: { "Content-type": "application/json" }
+    }
+    const req = await fetch(url, data);
+    this.res = await req.json();
+    console.log(this.res);
+
+    for (let i of this.res) {
+      if (i.correo == this.admin.email) {
+        console.log("nombre correcto");
+        if (i.contrasenia == this.admin.password) {
+          localStorage.setItem('id_admin', i.id_admin);
+          console.log("pass correcto");
+          this.router.navigateByUrl('/admin');
+        }
+      }
+      this.simulaAlert();
+    }
+  }
+
+  simulaAlert() {
+    this.datosIncorrectos = true;
+    setTimeout(() => {
+      this.datosIncorrectos = false;
+    }, 3000);
+  }
+  home(){
+    this.router.navigateByUrl('/home');
+  }
 }
