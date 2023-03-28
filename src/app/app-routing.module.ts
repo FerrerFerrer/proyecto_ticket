94e2a651fd1d0a46993c663e336b508a8dd101c8
FerrerFerrer <antonioferrer@uadec.edu.mx>
2023-03-27 23:27:49 -0600

    update barra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: InterAdminComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent }  
 ];
 
 @NgModule({
