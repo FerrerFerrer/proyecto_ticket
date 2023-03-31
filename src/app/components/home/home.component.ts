22a3158245063c88b44e01409ca29f2b96f215e9
¨Andrea <andreafuentes@uadec.edu.mx.com>
2023-03-31 00:19:58 -0600

    alert agregado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,12 +15,17 @@ export class HomeComponent {
 
   hacerticket() {
     this.router.navigateByUrl('/ticket')
-    
+
 
   }
   validarTurno() {
-    
+    if(this.buscar.curp.length < 18 ||  /\d/.test(this.buscar.curp.substring(0,4))){
+      console.log(this.buscar.curp.substring(0,3))
+      alert("CURP INCORRECTA!")
+    }else{
+      alert('CURP Correcta')
+    }
     
   }
-
+  
 }
